73a3f4519b15e2465814c1c8e53fa66dcad35062
Zuki Air <zukirust@gmail.com>
2024-10-09 15:57:42 +0100

    add launchscript.py a script which works like dmenu-run.
    the script works like dmenu-run but lets you add args for some programs
    or shortcuts in a config file. This is my workaround for the limitations
    of the dmenu-run script comparied to deskop files.

---
+++ launchscript.py
@@ -0,0 +1,53 @@
+#!/bin/env python3
+import os
+import subprocess
+import tomllib
+
+
+def configload(configpath):
+    with open(configpath, "rb") as f:
+        tomldata = tomllib.load(f)
+    return tomldata
+
+def getunixpathitems():
+    UNIXPATH = os.environ.get('PATH').rsplit(":")
+    UNIXPATHITEMS = []
+    for x in UNIXPATH:
+        if os.path.exists(x):
+            listdir = os.listdir(x)
+            for y in listdir:
+                file = (x + "/" + y)
+                if os.access(file, os.X_OK) and not os.path.isdir(file):
+                    #print(file + " is executable")
+                    UNIXPATHITEMS.append(y)
+
+    return UNIXPATHITEMS
+
+def main():
+    xdg_config_home = os.environ.get('XDG_CONFIG_HOME') or \
+         os.path.join(_home, '.config')
+    
+    configpath = xdg_config_home + "/launchscript.toml"
+    tomldata = configload(configpath)
+    tomldata_makeitems = tomldata["itemcreate"]
+    tomldata_modifyitems = tomldata["itemmodify"]
+    launcher = tomldata["options"]["launcher"]
+    modifyitems = tomldata_makeitems | tomldata_modifyitems
+    #print(modifyitems)
+
+    items = getunixpathitems()
+    items.sort()
+    itemclean = '\n'.join(items) # probably a cleaner way to do this
+    p = subprocess.run(['echo "' + itemclean + '" | ' + launcher], shell=True,capture_output=True,text=True)
+    launcherOutput = p.stdout.strip()
+    LAUNCH = ""
+    if launcherOutput in modifyitems:
+        LAUNCH = modifyitems[launcherOutput]
+    #elif ADD CASE FOR fake items
+    else:
+        LAUNCH = launcherOutput
+
+    subprocess.run([LAUNCH], shell=True) #without shell=True it will not take into account spaces
+
+if __name__ == "__main__":
+    main()
