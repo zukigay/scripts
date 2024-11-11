4807fc8593d273b8ed94512d3a307e4a531a35c0
Zuki Air <zukirust@gmail.com>
2024-11-11 16:32:03 +0000

    add mullvad_waybar patch even though i don't use it

--- mullvad_waybar.py
+++ mullvad_waybar.py
@@ -1,20 +1,39 @@
 #!/usr/bin/env python
+import re
 #import os
 import subprocess
 import json
+import sys
 
 mullcli = "mullvad"
 preface = "mull "
 
 def mullcall(mullvadcommand):
-    g = subprocess.run(mullvadcommand, capture_output=True,text=True)
+    g = subprocess.run(mullvadcommand, capture_output=True,text=True).stdout.strip()
     return g
 
 #def mull_get_state():
+
+
+def localemode(): # w.i.p 
+    relay_list = mullcall([mullcli, "relay", "list"])
+    county_lines = [line for line in relay_list.splitlines() if not re.search(r'	', line)]
+    relay_dict = {}
+    relay_list_codes = []
+    relay_list_names = []
+    for line in county_lines:
+        if not line == "":
+            line_split = line.split("(")
+            relay_list_codes.append(line_split[1])
+            relay_list_names.append(line_split[0])
+            relay_dict.update({line_split[0]: line_split[1]})
+    
+    locale_wanted = 20
+    print(relay_list_names[locale_wanted] + "(" + relay_list_codes[locale_wanted])
     
 
 def main():
-    mull_status_raw = mullcall([mullcli, "status", "-j"]).stdout.strip()
+    mull_status_raw = mullcall([mullcli, "status", "-j"])
     mull_status = json.loads(mull_status_raw)
     mull_state = mull_status['state']
     mull_locale = "UK"
@@ -33,5 +52,11 @@ def main():
 
 
 
+if len(sys.argv) > 1:
+    if sys.argv[1] == "l":
+        localemode()
+    else:
+        main()
+else:
+    main()
 
-main()
