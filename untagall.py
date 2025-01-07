9c212ecde4831e093431060665a1678f36e37092
Zuki Air <zukirust@gmail.com>
2025-01-07 15:37:13 +0000

    untagall.py: add support for user defined tags,and a command line flag for taging all windows with said tag instead.

--- untagall.py
+++ untagall.py
@@ -2,8 +2,26 @@
 import json
 import os
 import subprocess
- 
+import argparse
+
 def main():
+    args = parseArgs()
+    if args.tag == None:
+        print("no tag given, check help for proper usage, exiting...")
+        exit(1)
+    clientHandle(args.tag, args.tagAddMode)
+
+def parseArgs():
+    parser = argparse.ArgumentParser(
+                    prog='untagall',
+                    description='script to tag/untag all windows, using a user defined tag',
+                    epilog='specifying a tag with -t is requared!')
+    parser.add_argument('-t', '--tag', type=str)
+    parser.add_argument('-ta', '--tagAddMode',action='store_true',default=False)
+    args = parser.parse_args()
+    return args
+
+def clientHandle(targetTag, tagAddMode):
     p = subprocess.run(['hyprctl', 'clients', '-j'],capture_output=True,text=True)
     hyprctlClientData = p.stdout.strip()
     hyprctlClientDataJson = json.loads(hyprctlClientData)
@@ -11,9 +29,14 @@ def main():
     for client in hyprctlClientDataJson:
         clientAddress = client["address"]
         clientTags = client["tags"]
-        for tag in clientTags:
-            if tag == "overlay": # add support for using arg1 as tag input
-                hyprCommand = hyprCommand + f"dispatch tagwindow -overlay address:{clientAddress} ; "
+        for clientTag in clientTags:
+            if tagAddMode == False:
+                if clientTag == targetTag: # add support for using arg1 as tag input
+                    hyprCommand = hyprCommand + f"dispatch tagwindow -{targetTag} address:{clientAddress} ; "
+            else:
+                if clientTag != targetTag:
+                    hyprCommand = hyprCommand + f"dispatch tagwindow +{targetTag} address:{clientAddress} ; "
+
 
     if hyprCommand != "":
         print(f'hyprctl --batch "{hyprCommand}"') 
