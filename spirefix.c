8c20e6951e3311b3155cf0b8431e292d719c82fc
Zuki Air <zukirust@gmail.com>
2026-01-20 16:45:50 +0000

    update

--- spirefix.c
+++ spirefix.c
@@ -1,7 +1,12 @@
 #!/usr/bin/tcc -run -lX11
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
+#include <X11/Xutil.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 
 #define MAX(A, B)               ((A) > (B) ? (A) : (B))
@@ -11,28 +16,21 @@
 #define END(A)                  ((A) + LENGTH(A))
 
 typedef struct{
-    char * wm_name;
-    char * wm_class;
+    const char * wm_name;
+    const char * wm_class;
+    const char * process_id;
+    bool running;
 } Program;
 
 static Display *dpy;
 static Atom utf8;
 static Window root;
 
-// Program p[] = {
-//     {"Slay the Spire", "Slay the Spire"}
-// };
+Program prog_list[] = {
+    {"Slay the Spire", "Slay the Spire","SlayTheSpire",false},
+};
 
-
-
-int main() 
-{
-	if(!(dpy = XOpenDisplay(NULL))) {
-		fputs("spirefix: cannot open display\n", stderr);
-		return 1;
-	}
-
-    root = DefaultRootWindow(dpy);
+unsigned char * get_active() {
     Atom property = XInternAtom(dpy, "_NET_ACTIVE_WINDOW", False);
     //return values
     Atom type_return;
@@ -40,7 +38,6 @@ int main()
     unsigned long nitems_return;
     unsigned long bytes_left;
     unsigned char *data;
-	
 
     XGetWindowProperty(
         dpy,
@@ -56,64 +53,99 @@ int main()
         &bytes_left,    //should be 0 (i'm not sure but should be atomic read)
         &data           //should be non-null
     );
-    Window win = ((Window*) data)[0];
-    if (win != 0) {
-	    property = XInternAtom(dpy, "_NET_WM_NAME", True);
-	    unsigned char *p_str = NULL;
-	    Atom da;
-
-	    int di;
-	    unsigned long dl;
-	    utf8 = XInternAtom(dpy, "UTF8_STRING", True);
-	    XGetWindowProperty(dpy, win, property, 0, BUFSIZ, False, utf8, &da, &di, &dl, &dl, &p_str);
-        printf("_NET_WM_NAME %s\n",p_str);
+    return data;
+}
+char * get_prop(Window win,char * prop) {
+    Atom property = XInternAtom(dpy, prop, True);
+    unsigned char *p_str = NULL;
+    Atom da;
+    /* dl and di are unused */
+    unsigned long dl;
+    int di;
+    XGetWindowProperty(dpy, win, property, 0, BUFSIZ, False, utf8, &da, &di, &dl, &dl, &p_str);
+    return p_str;
+}
+int get_classname(Window win,unsigned char **str) {
+    XClassHint classhint;
+    Status ret = XGetClassHint(dpy,win,&classhint);
+    if (ret) {
+        *str = (unsigned char*)classhint.res_class;
+    } else {
+        *str = NULL;
     }
+    return ret;
+}
+/* todo remove all exec usage and switch to using native api's */
+void mute_prog(const char * prog_name,char * arg) {
+    char pgrep_arg[50];
+    snprintf(pgrep_arg,sizeof pgrep_arg, "pgrep %s",prog_name);
+    FILE * fp = popen(pgrep_arg,"r");
+    char pid[10];
+    fgets(pid,10,fp);
+    pclose(fp);
+    char * mute_args[] = {"wpctl","set-mute","-p",pid,arg,NULL};
+    if (fork() == 0) {
+        execvp(mute_args[0],mute_args);
+        exit(1);
+    }
+}
 
-    // printf("window %li\n",win);
 
-    XFree(data);
 
+int main() 
+{
+	if(!(dpy = XOpenDisplay(NULL))) {
+		fputs("spirefix: cannot open display\n", stderr);
+		return 1;
+	}
+
+    root = DefaultRootWindow(dpy);
+	utf8 = XInternAtom(dpy, "UTF8_STRING", True);
+    
+    if (root == 0) {fputs("No root window\n",stderr); return 1;}
+	
+
+	XSelectInput(dpy, root, PropertyChangeMask | StructureNotifyMask);
+	XEvent event;
+    for (;;) {
+	    fflush(stdout);
+	    XNextEvent(dpy, &event);
+ 	    if (event.type == DestroyNotify)
+ 		break;
+ 	    if (event.type != PropertyNotify)
+ 		continue;
+        unsigned char * d = get_active();
+        Window win = ((Window*) d)[0];
+        if (win != 0) {
+            unsigned char * wm_class;
+            if (get_classname(win,&wm_class)) {
+                char * wm_name = get_prop(win,"_NET_WM_NAME");
+                printf("wm_name %s wm_class %s\n",wm_name,wm_class);
+                Program * p;
+                for (p = prog_list; p < END(prog_list); p++) {
+                    if (strcmp(wm_name,p->wm_name) == 0 && strcmp(wm_class,p->wm_class) == 0) {
+                        printf("unmute %s\n",p->process_id);
+                        mute_prog(p->process_id,"0");
+                        p->running = true;
+                    } else if (p->running == true) {
+                        /* mute program */
+                        mute_prog(p->process_id,"1");
+                        printf("mute %s\n",p->process_id);
+                    }
+                }
+            }
+        } else { /* if there is no active window mute all running */
+            Program * p;
+            for (p = prog_list; p < END(prog_list); p++) {
+                if (p->running == true) {
+                    /* mute program */
+                    mute_prog(p->process_id,"1");
+                    printf("mute %s\n",p->process_id);
+                }
+            }
+        }
+        XFree(d);
+    }
 	XCloseDisplay(dpy);
     return 0;
 }
-
-/*
-datatype="class"
-targetclass=""
-while read type ; do 
-    # to add a game to this script simply add to this array first the class name (although only the first word of it with a prefixed "
-    # then add the games pid (note games with mutiable pids might not work
-    for data in '"Slay' SlayTheSpire '"Mosa' 'Mosa Lina'  ; do
-        if [ "$datatype" == "class" ] ; then
-            targetclass="$data"
-            echo "targetclass = $data" 
-            datatype="pid"
-            continue
-        else
-            pidname=$data
-            datatype="class"
-            echo "targetpid = $data" 
-        fi
-
-
-        pid="$(pgrep "$pidname")"
-        if [ "$pid" ] ; then
-            if [ "$type" = "_NET_ACTIVE_WINDOW(WINDOW): window id # 0x0" ]
-            then
-                wpctl set-mute -p "$(pgrep "$pidname")" 1
-            else
-            	set -- $type
-                classcmd=$(xprop -id "$5" WM_CLASS)
-                set -- $classcmd
-                class=$3
-                echo "class = $class"
-                if [ "$class" == "$targetclass" ] ; then
-                    wpctl set-mute -p "$pid" 0
-                else
-                    wpctl set-mute -p "$pid" 1
-                fi
-            fi
-        fi
-    done
-done < <(xprop -spy -root _NET_ACTIVE_WINDOW)
-*/
